f946f796d8228f358f65b7b3d1b636ae7f4a2144
Joseph Collins <joe@ijoe.co>
2012-08-28 02:09:02 -0400

    added zsh aliases for open command.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,11 @@ alias ~="cd ~/"
 alias ..="cd .."
 alias dotfiles="cd $ZSH"
 
+## Open With ##
+alias openf="open -a Finder"
+alias opent="open -a TextEdit"
+alias opens="open -a Safari"
+
 ## System Administrative ##
 alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles TRUE && killall Finder"
 alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles FALSE && killall Finder"
