4477514dc4122977d14db0dec4b4dca583ca7ce6
Joseph Collins <joe@ijoe.co>
2012-09-02 03:00:55 -0400

    Added a basic git remote alias, and git remotes with dropbox.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,6 +7,11 @@ alias gcam='git commit -a -m'
 alias gm='git merge'
 alias gco='git checkout'
 alias gb='git branch'
+alias gr='git remote'
+
+## Git Remotes With Dropbox ##
+alias grad='git remote add dropbox' // e.g. "grad ~/Dropbox/Your/Path/Here"
+alias gpud='git push -u dropbox' // e.g. "gpud master" sets remote branch and upstream
 
 ## Git Log Commands ##
 alias glg="git log --graph --all --pretty=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(black)%s%C(reset) %C(white)— %an%C(reset)%C(yellow)%d%C(reset)' --abbrev-commit --date=relative"
