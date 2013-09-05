e4e9b3202c0a82ac4cdcd715306c9454d0b0fb50
Joseph Collins <joe@ijoe.co>
2013-09-04 20:25:22 -0400

    Added git alias to clone a remote dropbox repo.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,6 +14,7 @@ alias gr='git remote'
 ## Git Remotes With Dropbox ##
 alias grad='git remote add dropbox' // e.g. "grad ~/Dropbox/Your/Path/Here"
 alias gpud='git push -u dropbox' // e.g. "gpud master" sets remote branch and upstream
+alias gcld='git clone -o dropbox' // e.g. "gcld ~/Dropbox/Your/Path/Here" Creates local repo and sets up remote called dropbox which auto-merges with master
 
 ## Git Log Commands ##
 alias glg="git log --graph --all --pretty=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(black)%s%C(reset) %C(white)— %an%C(reset)%C(yellow)%d%C(reset)' --abbrev-commit --date=relative"
