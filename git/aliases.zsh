a87baea66d620c7104dc35fdf655cd6d18d40200
Joseph Collins <joe@ijoe.co>
2013-09-11 17:07:53 -0400

    Added author's email to the end of the git log alias glg2.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,7 +19,7 @@ alias gcld='git clone -o dropbox' // e.g. "gcld ~/Dropbox/Your/Path/Here" Create
 ## Git Log Commands ##
 alias glg="git log --graph --all --pretty=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset) %C(black)%s%C(reset) %C(white)— %an%C(reset)%C(yellow)%d%C(reset)' --abbrev-commit --date=relative"
 
-alias glg2="git log --graph --all --format=format:'%C(blue)%h%C(reset) - %C(cyan)%aD%C(reset) %C(green)(%ar)%C(reset)%C(yellow)%d%C(reset)%n''          %C(black)%s%C(reset) %C(white)— %an%C(reset)' --abbrev-commit"
+alias glg2="git log --graph --all --format=format:'%C(blue)%h%C(reset) - %C(cyan)%aD%C(reset) %C(green)(%ar)%C(reset)%C(yellow)%d%C(reset)%n''          %C(black)%s%C(reset) %C(white)— %an%C(reset) <%ae%C(reset)>' --abbrev-commit"
 
 ## Git Status Commands ##
 alias gs='git status -sb'
