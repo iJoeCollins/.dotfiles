2165dd90ec4d60c76d5d07b2c6cafb8fc383c825
Joseph Collins <joe@ijoe.co>
2013-11-11 18:15:49 -0500

    Added open ST2 alias to zsh.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias dotfiles="cd $ZSH"
 alias openf="open -a Finder"
 alias opent="open -a TextEdit"
 alias opens="open -a Safari"
+alias openst="open -a Sublime\ Text\ 2"
 
 ## System Administrative ##
 alias show_hidden="defaults write com.apple.finder AppleShowAllFiles -bool TRUE && killall Finder"
