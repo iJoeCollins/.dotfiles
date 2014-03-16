eaaa15743579b479dcdf5f6fba830bdca7646128
Joseph Collins <joe@ijoe.co>
2014-03-16 16:09:48 -0400

    Updated window.zsh

--- zsh/window.zsh
+++ zsh/window.zsh
@@ -1,6 +1,13 @@
 # setup window title to show current dir and shell type
+#case $TERM in
+#    xterm*)
+#        precmd () {print -Pn "\e]0;%n: %~\a"}
+#        ;;
+#esac
+
+# setup window title to show machine name
 case $TERM in
     xterm*)
-        precmd () {print -Pn "\e]0;%n: %~\a"}
+        precmd () {print -Pn "\e]0;%m\a"}
         ;;
 esac
